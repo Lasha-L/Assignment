8c97df87ece862ccf903bf7aeda0746701081f07
Muza21 <lasha.muzashvili1@gmail.com>
2022-07-06 17:19:37 +0400

    improved sign up page with css and finished final page

---
+++ js/register2.js
@@ -0,0 +1,8 @@
+document.getElementById("next").onclick = function(){
+    location.href = "final.html"
+}
+
+document.getElementById("previous").onclick = function(){
+    location.href = "register1.html"
+}
+
