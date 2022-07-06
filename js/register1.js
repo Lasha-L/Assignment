3e40ca0d8c8a937717a21e04be9e98a79646bd6a
Muza21 <lasha.muzashvili1@gmail.com>
2022-07-06 23:03:45 +0400

    again added warning popup style only visual part and also other css on sign up page

--- js/register1.js
+++ js/register1.js
@@ -6,3 +6,21 @@ document.getElementById("previous").onclick = function(){
     location.href = "index.html"
 }
 
+function validateForm() {
+    var x = document.forms["form-group"].value;
+    if (x == "") {
+      let header = document.getElementById("warning-header");
+      header.innerText = "";
+      return false;
+    }
+  } 
+
+$("#btn").click(function () {
+
+    if ($("#frm")[0].checkValidity())
+        alert('sucess');
+    else
+        //Validate Form
+        $("#frm")[0].reportValidity()
+
+});
